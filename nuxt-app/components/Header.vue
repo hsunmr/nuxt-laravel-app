db4c69966036d4005a7912891da50b08618115dc
Hsun <hsunmr0823@gmail.com>
2021-10-21 18:28:42 +0800

    [nuxt] fix content

--- nuxt-app/components/Header.vue
+++ nuxt-app/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="bg-gray-700">
+    <nav class="bg-gray-700 fixed w-full z-10">
         <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div class="relative flex items-center justify-between h-16">
                 <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
-    index
+  <div class="relative container mx-auto h-full bg-gray-100 border-2 border-gray-300" style="width:728px;min-height:calc(100vh - 64px);top:70px">
+    <div>
+      content
+    </div>
   </div>
-
-
 </template>
 
 <script>
