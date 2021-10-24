2ece3e8cc31061c9593b817ad04f067d0de54958
Hsun <hsunmr0823@gmail.com>
2021-10-24 12:19:18 +0800

    [laravel] add posts api

---
+++ laravel-backend/app/Http/Controllers/PostController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Post::orderByDesc('id')->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $fields = $request->validate([
+            'title'   => 'required|string',
+            'content' => 'required|string',
+        ]);
+
+        Post::create([
+            'title'   => $fields['title'],
+            'content' => $fields['content'],
+            'user_id' => $request->user()->id,
+        ]);
+
+        return response([
+            'message' => 'success'
+        ],201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Post::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        $fields = $request->validate([
+            'title'   => 'required|string',
+            'content' => 'required|string',
+        ]);
+
+        $post->title   = $fields['title'];
+        $post->content = $fields['content'];
+        $post->save();
+
+        return response([
+            'message' => 'success'
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return response([
+            'message' => 'success'
+        ], 200);
+    }
+}
--- laravel-backend/routes/api.php
+++ laravel-backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,16 +15,26 @@ use App\Http\Controllers\AuthController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+// post
+
+Route::apiResource('posts', PostController::class)->only([
+   'store', 'update', 'destroy'
+])->middleware(['auth:api']);
+
+Route::apiResource('posts', PostController::class)->except([
+   'store', 'update', 'destroy'
+]);
+
 Route::group([
-    'middleware' => ['api', 'auth:api'],
-    'prefix' => 'auth'
- ], function ($router) {
+   'middleware' => ['api', 'auth:api'],
+   'prefix' => 'auth'
+], function ($router) {
 
-    Route::post('register', [AuthController::class, 'register'])->withoutMiddleware(['auth:api']);
-    Route::post('login', [AuthController::class, 'login'])->withoutMiddleware(['auth:api']);
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::post('refresh', [AuthController::class, 'refresh'])->withoutMiddleware(['auth:api']);
-    Route::get('user', [AuthController::class, 'user']);
-
- });
+   Route::post('register', [AuthController::class, 'register'])->withoutMiddleware(['auth:api']);
+   Route::post('login', [AuthController::class, 'login'])->withoutMiddleware(['auth:api']);
+   Route::post('logout', [AuthController::class, 'logout']);
+   Route::post('refresh', [AuthController::class, 'refresh'])->withoutMiddleware(['auth:api']);
+   Route::get('user', [AuthController::class, 'user']);
+});
 
