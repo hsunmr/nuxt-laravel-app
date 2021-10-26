9a3f8484065d48a1179f01acf74d8671851b6687
Hsun <hsun.chen@walkermedia.tw>
2021-10-26 18:43:28 +0800

    [nuxt] add posts create, edit, index page

---
+++ nuxt-app/pages/posts/create.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="relative container mx-auto h-full w-full sm:w-5/6 md:w-3/6 bg-gray-100 border-2 border-gray-300" style="top:70px">
+        <div>
+            create
+        </div>
+    </div>
+</template>
